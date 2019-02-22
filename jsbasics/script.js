0c03e855cbceeddd93f8b2cf94d4117b9c8c810c
pranamya <pranamyaab@gmail.com>
2019-02-22 22:32:07 +0530

    comment

---
+++ jsbasics/script.js
@@ -0,0 +1,93 @@
+//decision making
+/*var age=18;
+if(age<13){
+    console.log("minor");
+}else if(age>=18){
+    console.log("major");
+}
+age>=18?console.log("major"):console.log("minor");;
+function pranamya(firstname,lastname){
+    console.log(firstname+''+lastname);
+}
+pranamya('pranu','ab');
+
+var names=['pranu','2','haa']
+console.log(names[1]);
+
+names.push("hello");
+console.log(names);
+names.unshift("raju");
+console.log(names);
+names.pop();
+console.log(names);
+*/
+
+//challenge 2
+bill1=124;
+bill2=48;
+bill3=268;
+
+
+function tipc(bill){
+   
+    if(bill<50){
+    
+        console.log("the tip is");
+        tip1=bill*0.2;
+        console.log(tip1);
+       
+        tot=bill+tip1;
+        console.log("the tot is");
+        console.log(tot);
+    }else if(bill>50 && bill<200){
+        tip2=bill*0.15;
+        console.log("the tip is");console.log(tip2);
+        
+        tot=bill+tip2;
+        
+        console.log("the tot is");
+
+        console.log(tot);
+    }else if(bill>200){
+        tip3=bill*0.1;
+        console.log("the tip is");
+        console.log(tip3);
+        
+        tot=bill+tip3;
+        
+        console.log("the tot is");
+
+        console.log(tot);
+    }
+    
+}
+
+tipc(124);
+tipc(48);
+tipc(268);
+
+
+/*
+//objects
+var mydetails={
+    firstname:'pranu',
+    lastname:'ab',
+    dob:1998,
+    family:['pranu','ab'],
+    calcage:function(birthvar){
+        return 2019-birthvar;
+    }
+};
+
+console.log(mydetails.firstname);
+var z='dob';
+console.log(mydetails[z]);
+
+mydetails.dob=1981;
+console.log(mydetails);
+
+var shiva=new Object();
+shiva.firstname='deep';
+console.log(shiva);
+
+console.log('pranu is'+mydetails.calcage(1998)+'years old');*/
